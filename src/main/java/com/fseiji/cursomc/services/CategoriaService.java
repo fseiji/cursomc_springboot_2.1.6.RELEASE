d0b16755f46ac1c375bc005cf474089b3ad71d57
Fernando Seiji <fseiji@gmail.com>
2019-07-11 22:09:55 -0300

    estado e cidade

--- src/main/java/com/fseiji/cursomc/services/CategoriaService.java
+++ src/main/java/com/fseiji/cursomc/services/CategoriaService.java
@@ -17,10 +17,7 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		if (!obj.isPresent()) {
-			throw new ObjectNotFoundException(
-					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
-		}
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
