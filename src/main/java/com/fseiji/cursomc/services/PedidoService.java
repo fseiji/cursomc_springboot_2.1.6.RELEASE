e8829e56f0a88d4740dda722bdeecf088acc920f
Fernando Seiji <fseiji@gmail.com>
2019-07-30 21:00:32 -0300

    atualizando categoria put e outros pequenos ajustes

--- src/main/java/com/fseiji/cursomc/services/PedidoService.java
+++ src/main/java/com/fseiji/cursomc/services/PedidoService.java
@@ -15,7 +15,7 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = this.repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
