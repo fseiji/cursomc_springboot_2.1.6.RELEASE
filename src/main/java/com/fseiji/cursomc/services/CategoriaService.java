04734ae55cb93e1b793ec444753b5e6b469b3e2f
Fernando Seiji <fseiji@gmail.com>
2019-08-01 21:30:41 -0300

    listando todas categorias com dto

--- src/main/java/com/fseiji/cursomc/services/CategoriaService.java
+++ src/main/java/com/fseiji/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.fseiji.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,10 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 	public Categoria update(Categoria obj) {
