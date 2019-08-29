5c8859801592debb1cfa8c30cfbd44995f68c1c4
Fernando Seiji <fseiji@gmail.com>
2019-08-28 21:22:53 -0300

    validacao customizada: email nao repetido na atualizacao de cliente

---
+++ src/main/java/com/fseiji/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,55 @@
+package com.fseiji.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.fseiji.cursomc.domain.Cliente;
+import com.fseiji.cursomc.domain.enums.TipoCliente;
+import com.fseiji.cursomc.dto.ClienteDTO;
+import com.fseiji.cursomc.dto.ClienteNewDTO;
+import com.fseiji.cursomc.repositories.ClienteRepository;
+import com.fseiji.cursomc.resources.exception.FieldMessage;
+import com.fseiji.cursomc.services.validation.utils.BR;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+				
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente cliente = this.repo.findByEmail(objDto.getEmail());
+		if(cliente != null && !uriId.equals(cliente.getId())) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
