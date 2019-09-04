c4b910b2b07198b9756c392b26b40147f908e92f
Fernando Seiji <fseiji@gmail.com>
2019-09-03 22:07:30 -0300

    inserindo pedido

--- src/main/java/com/fseiji/cursomc/domain/ItemPedido.java
+++ src/main/java/com/fseiji/cursomc/domain/ItemPedido.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class ItemPedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
@@ -30,18 +30,26 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
-	}	
-	
+	}
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 	public ItemPedidoPK getId() {
--- src/main/java/com/fseiji/cursomc/services/PedidoService.java
+++ src/main/java/com/fseiji/cursomc/services/PedidoService.java
@@ -1,11 +1,19 @@
 package com.fseiji.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fseiji.cursomc.domain.ItemPedido;
+import com.fseiji.cursomc.domain.PagamentoComBoleto;
 import com.fseiji.cursomc.domain.Pedido;
+import com.fseiji.cursomc.domain.enums.EstadoPagamento;
+import com.fseiji.cursomc.repositories.ItemPedidoRepository;
+import com.fseiji.cursomc.repositories.PagamentoRepository;
 import com.fseiji.cursomc.repositories.PedidoRepository;
 import com.fseiji.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +23,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+
+	@Autowired
+	private BoletoService boletoService;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = this.repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pgto = (PagamentoComBoleto) obj.getPagamento();
+			this.boletoService.preencherPagamentoComBoleto(pgto, obj.getInstante());
+		}
+		obj = this.repo.save(obj);
+		this.pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(this.produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		this.itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
