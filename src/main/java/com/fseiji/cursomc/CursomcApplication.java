829b4c7228f06d4ec103342edd747ed51dfa7703
Fernando Seiji <fseiji@gmail.com>
2019-10-29 21:09:43 -0300

    enviando imagem via endpoint

--- src/main/java/com/fseiji/cursomc/CursomcApplication.java
+++ src/main/java/com/fseiji/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.fseiji.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fseiji.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,6 +13,5 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Development\\curso-udemy\\workspace\\cursomc\\src\\main\\java\\com\\fseiji\\cursomc\\imagens\\cp1.jpg");
 	}
 }
